7fdf7c2a16a63882983ab56656ed18f7f01be3c1
yasminemagdy <yasmenmagdy640@gmail.com>
2021-02-27 19:29:09 +0400

    m

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,23 +2,42 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const workboxplugin = require('workbox-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client',
+        path: path.resolve(__dirname, 'dist')
+    },
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: '/\.scss$/',
+                use: [MiniCssExtractPlugin.loader, 'css-loader' , 'sass-loader']
+            },
+            {
+
             }
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new workboxplugin.GenerateSW()
+        new workboxplugin.GenerateSW({
+            clientsClaim: true,
+            skipWaiting: true
+        }),
+        new MiniCssExtractPlugin({filename: "[name].css"})
     ]
 }
