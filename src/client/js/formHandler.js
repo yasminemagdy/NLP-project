bd87fdc75f748e050eaaea7d4972bd5d8c4ca663
yasminemagdy <yasmenmagdy640@gmail.com>
2021-02-28 21:30:43 +0400

    n

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -5,8 +5,14 @@ function handleSubmit(event) {
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    fetch('http://localhost:8080/test', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        mode: 'cors',
+        body: JSON.stringify({
+            url: formText
+        })
+    })
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
