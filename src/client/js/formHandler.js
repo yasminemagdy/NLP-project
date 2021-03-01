6303bf3ea837b95f7e5a868656b346f8534d6189
yasminemagdy <yasmenmagdy640@gmail.com>
2021-03-01 17:43:19 +0400

    attempt to validate the URL

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,7 +3,7 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+   if(Client.checkForName(formText)){
 
     fetch('http://localhost:4040/test', {
         method: 'POST',
@@ -18,6 +18,13 @@ function handleSubmit(event) {
         console.log(res)
     document.getElementById('results').innerHTML = `agreement: ${res.agreement} <br> model: ${res.model} <br> confidence: ${res.confidence} <br> irony: ${res.irony} <br> score_tag: ${res.score_tag} <br> subjectivity: ${res.subjectivity}`
     })
+     }else{
+         if(!formText == ''){
+             alert('Please, Enter a valid URL')
+         }else{
+             alert("There is no test on search field!, Fill it")
+         }
+     }
 }
 
 export { handleSubmit }
